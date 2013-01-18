4e6497a628905cb700202f8c958977a4c9006d4b
aymeric PICOU <aymeric.picou@sfr.fr>
2013-01-18 11:00:34 +0100

    projet intermodule :
    On a arrive à faire communiquer les pages web avec la base de données, mais avec des échanges de messages.
    Le but de la prochaine version est de faire la même chose, mais avec des interfaces remote.

---
+++ Documents/NetBeansProjects/EJB3/EJB3-ejb/src/java/ejb/NewsEntityFacade.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author aymeric
+ */
+@Stateless
+public class NewsEntityFacade extends AbstractFacade<NewsEntity> {
+    @PersistenceContext(unitName = "EJB3-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public NewsEntityFacade() {
+        super(NewsEntity.class);
+    }
+    
+}
